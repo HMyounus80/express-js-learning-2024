c4f6aeff430492014ef441c6943102db784697f6
HMyounus80 <younusm137@gmail.com>
2024-02-11 16:32:35 +0600

    router practice

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 const express = require('express'); 
 
 const app = express(); 
-const PORT = 3000; 
+const PORT = 3000;
+app.use(express.json()); 
 
+// Route configuration
+// app.get('/', (req, res)=>{ 
+//     res.status(200); 
+//     res.send("Welcome to root URL of Server"); 
+// })
+
+// html template route configuration
+app.get('/hello', (req, res)=>{ 
+    res.set('Content-Type', 'text/html'); 
+    res.status(200).send("<h1>Hello Express JS Learner!</h1>"); 
+});
+
+// post route with send data to server
+
+app.post('/', (req, res)=>{ 
+    const {name} = req.body; 
+      
+    res.send(`Welcome ${name}`); 
+}) 
+
+
+
+// app listening
 app.listen(PORT, (error) =>{ 
 	if(!error) 
 		console.log("Server is Successfully Running and App is listening on port"+ PORT) 
